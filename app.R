128466049b03f1912d13a7b895ed8a38c621948a
riccymiccy <richard_m_clark@hotmail.co.uk>
2020-04-22 15:49:04 +0100

    adds eruption counter - app checked working

--- app.R
+++ app.R
@@ -2,8 +2,10 @@ library(shiny)
 library(tidyverse)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  
   # Application title
   titlePanel("Old Faithful Geyser Data"),
+  
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
@@ -13,14 +15,27 @@ ui <- fluidPage(
                   max = 50,
                   value = 30)
     ),
+    
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput(outputId = "distPlot")
+      plotOutput(outputId = "distPlot"),
+      # ----
+      # ADD THIS
+      # ----
+      textOutput(outputId = "eruptionCounter")
     )
   )
 )
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  # ----
+  # ADD THIS
+  # This isn't particularly good code, we probably wouldn't call this data.
+  # We're just doing it to make sure a git conflict happens later!
+  # ----
+  data <- faithful %>%
+    summarise(num_eruptions = n())
   
   output$distPlot <- renderPlot({
     # draw the histogram with the specified number of bins
@@ -29,6 +44,13 @@ server <- function(input, output) {
       xlab("Waiting time (mins)") +
       ylab("Number of eruptions") +
       ggtitle("Histogram of eruption waiting times")
+  })
+  
+  # ----
+  # ADD THIS
+  # ----
+  output$eruptionCounter <- renderText({
+    paste("Number of eruptions in histogram: ", data$num_eruptions)
   })
   
 }
